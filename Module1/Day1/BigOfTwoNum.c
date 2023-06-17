2184368daa464bff0c2d2c816938a04008978cf8
Kshithijb20 <91237339+Kshithijb20@users.noreply.github.com>
2023-06-16 20:15:09 -0600

    add Module1

---
+++ Module1/Day1/BigOfTwoNum.c
@@ -0,0 +1,16 @@
+
+int a,b;
+int biggest(a,b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+
+// using ternary operator
+
+int biggest2 (int a, int b) {
+    return (a > b) ? a : b;
+}
