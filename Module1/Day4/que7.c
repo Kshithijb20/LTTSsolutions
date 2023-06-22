c293167f60ef4bb65ddb951b435f9b63cb3fdc8a
Kshithijb20 <91237339+Kshithijb20@users.noreply.github.com>
2023-06-22 00:51:05 -0600

    add Day4

---
+++ Module1/Day4/que7.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX_SIZE 100
+
+void toggleCase(char *str) {
+    int length = strlen(str);
+
+    for (int i = 0; i < length; i++) {
+        if (islower(str[i])) {
+            str[i] = toupper(str[i]);
+        } else if (isupper(str[i])) {
+            str[i] = tolower(str[i]);
+        }
+    }
+}
+
+int main() {
+    char str[MAX_SIZE];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Remove trailing newline character
+    str[strcspn(str, "\n")] = '\0';
+
+    printf("Original string: %s\n", str);
+
+    toggleCase(str);
+
+    printf("Toggled case string: %s\n", str);
+
+    return 0;
+}
